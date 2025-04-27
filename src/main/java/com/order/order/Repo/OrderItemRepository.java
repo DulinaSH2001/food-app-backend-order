32570a0c95cd1ce8625c1999c2e5e81671e6240e
Dulina Hejitha <128177354+DulinaSH2001@users.noreply.github.com>
2025-04-27 14:22:09 +0530

    added payment and order config

--- src/main/java/com/order/order/Repo/OrderItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.order.order.Repo;
-
-import com.order.order.Model.OrderItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
-}
